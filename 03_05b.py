1f93fcad23dc1cd680336abac24883d809889bc6
shevious <shevious@yahoo.com>
2020-10-23 23:35:41 +0900

    face vae added

---
+++ 03_05b.py
@@ -0,0 +1,195 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Conv2D, LeakyReLU, Flatten
+from tensorflow.keras.layers import Dense, Conv2DTranspose
+from tensorflow.keras.layers import Reshape, Activation
+from tensorflow.keras.layers import BatchNormalization, Dropout
+from tensorflow.keras import backend as K
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import LearningRateScheduler
+
+import os
+from glob import glob
+
+#from models.VAE import VariationalAutoencoder
+#from utils.loaders import load_mnist
+
+# run params
+section = 'vae'
+run_id = '0001'
+data_name = 'faces'
+RUN_FOLDER = 'run/{}/'.format(section)
+RUN_FOLDER += '_'.join([run_id, data_name])
+
+if not os.path.exists(RUN_FOLDER):
+    os.mkdir(RUN_FOLDER)
+    os.mkdir(os.path.join(RUN_FOLDER, 'viz'))
+    os.mkdir(os.path.join(RUN_FOLDER, 'images'))
+    os.mkdir(os.path.join(RUN_FOLDER, 'weights'))
+
+DATA_FOLDER = './data/celeb/'
+
+INPUT_DIM = (128,128,3)
+BATCH_SIZE = 32
+
+filenames = np.array(glob(os.path.join(DATA_FOLDER, '*/*.jpg')))
+
+NUM_IMAGES = len(filenames)
+
+
+mode =  'build' #'load' #
+#mode =  'load' 
+
+data_gen = ImageDataGenerator(rescale=1./255)
+
+data_flow = data_gen.flow_from_directory(DATA_FOLDER
+                                         , target_size = INPUT_DIM[:2]
+                                         , batch_size = BATCH_SIZE
+                                         , shuffle = True
+                                         , class_mode = 'input'
+                                         , subset = "training"
+                                            )
+
+
+class Sampling(layers.Layer):
+    """Uses (mu, log_var) to sample z, the vector encoding a digit."""
+
+    def call(self, inputs):
+        mu, log_var = inputs
+        batch = tf.shape(mu)[0]
+        dim = tf.shape(mu)[1]
+        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+        return mu + tf.exp(log_var/2) * epsilon
+
+
+z_dim = 200
+r_loss_factor = 10000
+
+encoder_input = keras.Input(shape=INPUT_DIM, name='encoder_input')
+x = Conv2D(32, 3, strides=2, padding="same", name='encoder_conv_0')(encoder_input)
+x = BatchNormalization()(x)
+x = LeakyReLU()(x)
+x = Conv2D(64, 3, strides=2, padding="same", name='encoder_conv_1')(x)
+x = BatchNormalization()(x)
+x = LeakyReLU()(x)
+x = Dropout(rate = 0.25)(x)
+x = Conv2D(64, 3, strides=2, padding="same", name='encoder_conv_2')(x)
+x = BatchNormalization()(x)
+x = LeakyReLU()(x)
+x = Dropout(rate = 0.25)(x)
+x = Conv2D(64, 3, strides=2, padding="same", name='encoder_conv_3')(x)
+x = BatchNormalization()(x)
+x = Dropout(rate = 0.25)(x)
+x = LeakyReLU()(x)
+x = Dropout(rate = 0.25)(x)
+shape_before_flattening = K.int_shape(x)[1:]
+x = Flatten()(x)
+mu = Dense(z_dim, name='mu')(x)
+log_var = Dense(z_dim, name='log_var')(x)
+z = Sampling(name='encoder_output')([mu, log_var])
+encoder = keras.Model(encoder_input, [mu, log_var, z], name = 'encoder')
+
+decoder_input = keras.Input(shape=(z_dim,), name='decoder_input')
+x = Dense(np.prod(shape_before_flattening))(decoder_input)
+x = Reshape(shape_before_flattening)(x)
+x = layers.Conv2DTranspose(64, 3, strides=2, padding="same", name='decoder_conv_t0')(x)
+x = BatchNormalization()(x)
+x = LeakyReLU()(x)
+x = Dropout(rate = 0.25)(x)
+x = layers.Conv2DTranspose(64, 3, strides=2, padding="same", name='decoder_conv_t1')(x)
+x = BatchNormalization()(x)
+x = LeakyReLU()(x)
+x = Dropout(rate = 0.25)(x)
+x = layers.Conv2DTranspose(32, 3, strides=2, padding="same", name='decoder_conv_t2')(x)
+x = BatchNormalization()(x)
+x = LeakyReLU()(x)
+x = Dropout(rate = 0.25)(x)
+x = layers.Conv2DTranspose(3, 3, strides=2, padding="same", name='decoder_conv_t3')(x)
+decoder_output = Activation('sigmoid')(x)
+decoder = keras.Model(decoder_input, decoder_output, name="decoder")
+
+encoder.summary()
+decoder.summary()
+
+class VAEModel(keras.Model):
+    def __init__(self, encoder, decoder, **kwargs):
+        super(VAEModel, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        #self.r_loss_factor = r_loss_factor
+
+    def train_step(self, data):
+        if isinstance(data, tuple):
+            data = data[0]
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, z = self.encoder(data)
+            reconstruction = self.decoder(z)
+            #z_mean, z_log_var, z = encoder(data)
+            #reconstruction = decoder(z)
+            reconstruction_loss = tf.reduce_mean(
+                tf.square(data - reconstruction), axis = [1,2,3]
+                #keras.losses.binary_crossentropy(data, reconstruction)
+            )
+            reconstruction_loss *= r_loss_factor
+            #reconstruction_loss *= 28 * 28
+            kl_loss = 1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)
+            kl_loss = tf.reduce_sum(kl_loss, axis=1)
+            kl_loss *= -0.5
+            total_loss = reconstruction_loss + kl_loss
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        return {
+            "loss": tf.reduce_mean(total_loss),
+            "reconstruction_loss": tf.reduce_mean(reconstruction_loss),
+            "kl_loss": tf.reduce_mean(kl_loss),
+        }
+
+    def call(self,inputs):
+        latent = self.encoder(inputs)
+        return self.decoder(latent)
+
+LEARNING_RATE = 0.0005
+EPOCHS = 200
+PRINT_EVERY_N_BATCHES = 100
+INITIAL_EPOCH = 0
+
+save_folder = os.path.join(RUN_FOLDER, 'weights')
+
+VAE = VAEModel(encoder, decoder)
+VAE.compile(optimizer=keras.optimizers.Adam(lr=LEARNING_RATE))
+
+#MODE = 'build'
+MODE = 'load'
+
+if MODE == 'load':
+    #ae = keras.models.load_model(save_folder, custom_objects={'r_loss': r_loss})
+    VAE.load_weights(save_folder+'/'+'checkpoint')
+
+def step_decay_schedule(initial_lr, decay_factor=0.5, step_size=1):
+    '''
+    Wrapper function to create a LearningRateScheduler with step decay schedule.
+    '''
+    def schedule(epoch):
+        new_lr = initial_lr * (decay_factor ** np.floor(epoch/step_size))
+        return new_lr
+
+    return LearningRateScheduler(schedule)
+ 
+checkpoint = ModelCheckpoint(save_folder+'/'+'checkpoint', save_weights_only = False, verbose=1)
+lr_sched = step_decay_schedule(initial_lr=LEARNING_RATE, decay_factor=1, step_size=1)
+callbacks_list = [checkpoint, lr_sched]
+
+VAE.fit(
+    data_flow,
+    shuffle=True,
+    epochs=EPOCHS,
+    steps_per_epoch = NUM_IMAGES / BATCH_SIZE,
+    callbacks=callbacks_list
+)
+
+x = np.random.rand(1,128,128,3)
+VAE.predict(x)
+VAE.save(save_folder)
