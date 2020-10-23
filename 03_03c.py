a5eb4d1f4c8ace550138d8f7672d4a14b023aec8
shevious <shevious@yahoo.com>
2020-10-23 23:31:50 +0900

    03_03c cross entroy

---
+++ 03_03c.py
@@ -0,0 +1,165 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Conv2D, LeakyReLU, Flatten
+from tensorflow.keras.layers import Dense, Conv2DTranspose
+from tensorflow.keras.layers import Reshape, Activation
+from tensorflow.keras import backend as K
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import LearningRateScheduler
+
+import os
+
+#from models.VAE import VariationalAutoencoder
+#from utils.loaders import load_mnist
+
+# run params
+SECTION = 'vae'
+RUN_ID = '0003'
+DATA_NAME = 'digits'
+RUN_FOLDER = 'run/{}/'.format(SECTION)
+RUN_FOLDER += '_'.join([RUN_ID, DATA_NAME])
+
+if not os.path.exists(RUN_FOLDER):
+    os.mkdir(RUN_FOLDER)
+    os.mkdir(os.path.join(RUN_FOLDER, 'viz'))
+    os.mkdir(os.path.join(RUN_FOLDER, 'images'))
+    os.mkdir(os.path.join(RUN_FOLDER, 'weights'))
+
+#mode =  'build' #'load' #
+mode =  'load' #'load' #
+
+class Sampling(layers.Layer):
+    """Uses (mu, log_var) to sample z, the vector encoding a digit."""
+
+    def call(self, inputs):
+        mu, log_var = inputs
+        #batch = tf.shape(mu)[0]
+        #dim = tf.shape(mu)[1]
+        #epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+        epsilon = tf.keras.backend.random_normal(shape=tf.shape(mu))
+        return mu + tf.exp(log_var/2) * epsilon
+
+
+z_dim = 2
+r_loss_factor = 1000
+
+latent_dim = z_dim
+
+encoder_input = keras.Input(shape=(28, 28, 1), name='encoder_input')
+x = Conv2D(32, 3, strides=1, padding="same", name='encoder_conv_0')(encoder_input)
+x = LeakyReLU()(x)
+x = Conv2D(64, 3, strides=2, padding="same", name='encoder_conv_1')(x)
+x = LeakyReLU()(x)
+x = Conv2D(64, 3, strides=2, padding="same", name='encoder_conv_2')(x)
+x = LeakyReLU()(x)
+x = Conv2D(64, 3, strides=1, padding="same", name='encoder_conv_3')(x)
+x = LeakyReLU()(x)
+shape_before_flattening = K.int_shape(x)[1:]
+x = Flatten()(x)
+mu = Dense(z_dim, name='mu')(x)
+log_var = Dense(z_dim, name='log_var')(x)
+z = Sampling(name='encoder_output')([mu, log_var])
+encoder = keras.Model(encoder_input, [mu, log_var, z], name = 'encoder')
+
+decoder_input = keras.Input(shape=(z_dim,), name='decoder_input')
+x = Dense(np.prod(shape_before_flattening))(decoder_input)
+x = Reshape(shape_before_flattening)(x)
+x = layers.Conv2DTranspose(64, 3, strides=1, padding="same", name='decoder_conv_t0')(x)
+x = LeakyReLU()(x)
+x = layers.Conv2DTranspose(64, 3, strides=2, padding="same", name='decoder_conv_t1')(x)
+x = LeakyReLU()(x)
+x = layers.Conv2DTranspose(32, 3, strides=2, padding="same", name='decoder_conv_t2')(x)
+x = LeakyReLU()(x)
+x = layers.Conv2DTranspose(1, 3, strides=1, padding="same", name='decoder_conv_t3')(x)
+decoder_output = Activation('sigmoid')(x)
+decoder = keras.Model(decoder_input, decoder_output, name="decoder")
+
+class VAEModel(keras.Model):
+    def __init__(self, encoder, decoder, **kwargs):
+        super(VAEModel, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        #self.r_loss_factor = r_loss_factor
+
+    def train_step(self, data):
+        if isinstance(data, tuple):
+            data = data[0]
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, z = self.encoder(data)
+            reconstruction = self.decoder(z)
+            #z_mean, z_log_var, z = encoder(data)
+            #reconstruction = decoder(z)
+            reconstruction_loss = tf.reduce_mean(
+                #tf.square(data - reconstruction), axis = [1,2,3]
+                keras.losses.binary_crossentropy(data, reconstruction)
+            )
+            reconstruction_loss *= r_loss_factor
+            #reconstruction_loss *= 28 * 28
+            kl_loss = 1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)
+            #kl_loss = tf.reduce_sum(kl_loss, axis=1)
+            kl_loss = tf.reduce_sum(kl_loss, axis=1)
+            kl_loss *= -0.5
+            kl_loss = tf.reduce_mean(kl_loss)
+            total_loss = reconstruction_loss + kl_loss
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        return {
+            #"loss": tf.reduce_mean(total_loss),
+            #"reconstruction_loss": tf.reduce_mean(reconstruction_loss),
+            #"kl_loss": tf.reduce_mean(kl_loss),
+            "loss": total_loss,
+            "reconstruction_loss": reconstruction_loss,
+            "kl_loss": kl_loss,
+        }
+
+    def call(self,inputs):
+        latent = self.encoder(inputs)
+        return self.decoder(latent)
+
+(x_train, _), (x_test, _) = keras.datasets.mnist.load_data()
+x_train = np.concatenate([x_train, x_test], axis=0)
+x_train = np.expand_dims(x_train, -1).astype("float32") / 255
+
+LEARNING_RATE = 0.0005
+BATCH_SIZE = 128
+EPOCHS = 30
+PRINT_EVERY_N_BATCHES = 100
+INITIAL_EPOCH = 0
+save_folder = os.path.join(RUN_FOLDER, 'weights')
+
+VAE = VAEModel(encoder, decoder)
+VAE.compile(optimizer=keras.optimizers.Adam(lr=LEARNING_RATE))
+
+MODE = 'build'
+#MODE = 'load'
+
+if MODE == 'load':
+    #ae = keras.models.load_model(save_folder, custom_objects={'r_loss': r_loss})
+    VAE.load_weights(save_folder+'/'+'checkpoint')
+
+def step_decay_schedule(initial_lr, decay_factor=0.5, step_size=1):
+    '''
+    Wrapper function to create a LearningRateScheduler with step decay schedule.
+    '''
+    def schedule(epoch):
+        new_lr = initial_lr * (decay_factor ** np.floor(epoch/step_size))
+        return new_lr
+
+    return LearningRateScheduler(schedule)
+
+checkpoint = ModelCheckpoint(save_folder+'/'+'checkpoint', save_weights_only = False, verbose=1)
+lr_sched = step_decay_schedule(initial_lr=LEARNING_RATE, decay_factor=1, step_size=1)
+callbacks_list = [checkpoint, lr_sched]
+
+VAE.fit(
+    x_train,
+    shuffle=True,
+    epochs=EPOCHS,
+    batch_size=BATCH_SIZE,
+    callbacks=callbacks_list
+)
+
+VAE.predict(x_train[0:1])
+VAE.save(save_folder)
